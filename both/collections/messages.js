c022107beb97315279fa7924f3f323c407dd287f
Diego de Souza <diegotrus7@gmail.com>
2015-06-14 20:48:53 -0400

    Useraccounts update to use username and email for signup

--- both/collections/messages.js
+++ both/collections/messages.js
@@ -1,18 +1,5 @@
 Messages = new Mongo.Collection('messages');
 
-// Meteor.methods({
-  // 'Messages.save': function(body) {
-    // var user = Meteor.user();
-    // // var currentUserId = Meteor.user().emails[0].address;
-
-    // Messages.insert({
-      // body: body,
-      // createdAt: new Date(),
-      // createdBy: user.username
-    // });
-  // }
-// });
-
 Meteor.methods({
  'messageInsert': function(body) {
     var user = Meteor.user();
@@ -20,7 +7,7 @@ Meteor.methods({
     Messages.insert({
       message: body,
       username: user.username,
-      submitted: new Date()
+      createdAt: new Date()
     });
   }
 });
